59b5a8548022e30a7c2f6bbddc6120d4d8249761
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2021-07-04 01:31:43 +0300

    Update CartList.js

--- app/src/CartList.js
+++ app/src/CartList.js
@@ -2,19 +2,20 @@ import React, { Component } from "react";
 import { Table, Button } from "reactstrap";
 
 export default class CartList extends Component {
-  renderCart() {
+  renderList() {
     return (
       <tbody>
         {this.props.cart.map((cartItem) => (
           <tr key={cartItem.product.id}>
             <th scope="row">{cartItem.product.id}</th>
-            <td>{cartItem.product.productName}</td>
+            <td>{cartItem.product.name}</td>
+            <td>{cartItem.product.unitPrice}</td>
             <td>{cartItem.quantity}</td>
-            <td>{cartItem.product.unitPrice} $</td>
-            <td>{cartItem.product.unitPrice * cartItem.quantity} $</td>
+            <td>{cartItem.product.unitPrice * cartItem.quantity}</td>
             <td>
               <Button
                 color="danger"
+                outline
                 onClick={() => this.props.removeFromCart(cartItem.product)}
               >
                 X
@@ -46,13 +47,13 @@ export default class CartList extends Component {
             <tr>
               <th>#</th>
               <th>Product Name</th>
-              <th>Quantity</th>
               <th>Unit Price</th>
-              <th>Line Total</th>
-              <th></th>
+              <th>Quantity</th>
+              <th>Total Line</th>
+              <td></td>
             </tr>
           </thead>
-          {this.props.cart.length > 0 ? this.renderCart() : this.renderEmpty()}
+          {this.props.cart.length > 0 ? this.renderList() : this.renderEmpty()}
         </Table>
       </div>
     );
