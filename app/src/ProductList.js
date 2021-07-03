6b69fc99674010e74a6dd6664e0068b4aa6cae5f
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2021-07-04 01:33:01 +0300

    Update ProductList.js

--- app/src/ProductList.js
+++ app/src/ProductList.js
@@ -5,21 +5,17 @@ export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <Badge color="primary" pill>
-          <h4>{this.props.info.title}</h4>
-        </Badge>{" "}
-        <Badge color="warning" pill>
-          <h4>{this.props.currentCategory}</h4>
-        </Badge>
-        <br /> <br />
+        <h2>
+          <Badge color="primary">{this.props.info.title}</Badge> -{" "}
+          <Badge color="warning">{this.props.currentCategory}</Badge>
+        </h2>
         <Table hover>
           <thead>
             <tr>
               <th>#</th>
               <th>Product Name</th>
-              <th>Quantity Per Unit</th>
-              <th>Units In Stock</th>
               <th>Unit Price</th>
+              <th>Units In Stock</th>
               <th></th>
             </tr>
           </thead>
@@ -27,10 +23,9 @@ export default class ProductList extends Component {
             {this.props.products.map((product) => (
               <tr key={product.id}>
                 <th scope="row">{product.id}</th>
-                <td>{product.productName}</td>
-                <td>{product.quantityPerUnit}</td>
+                <td>{product.name}</td>
+                <td>{product.unitPrice}</td>
                 <td>{product.unitsInStock}</td>
-                <td>{product.unitPrice} $</td>
                 <td>
                   <Button
                     color={product.unitsInStock > 0 ? "primary" : "danger"}
@@ -38,8 +33,7 @@ export default class ProductList extends Component {
                     onClick={() => this.props.addToCart(product)}
                     disabled={product.unitsInStock > 0 ? false : true}
                   >
-                    {product.unitsInStock > 0 ? "Add" : "OoS"}{" "}
-                    {/* Out of stock */}
+                    {product.unitsInStock > 0 ? "Add" : "OoS"}
                   </Button>
                 </td>
               </tr>
